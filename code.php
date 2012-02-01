b2bdf08c5c7591e35330436976f10ad6e277896a
Jukka Svahn <web@rahinaa.biz>
2012-02-01 04:25:11 +0200

    Adding v0.1 from SVN.

---
+++ code.php
@@ -0,0 +1,37 @@
+<?php	##################
+	#
+	#	rah_runtime-plugin for Textpattern
+	#	version 0.1
+	#	by Jukka Svahn
+	#	http://rahforum.biz
+	#
+	#	Copyright (C) 2010 Jukka Svahn
+	#	Licensed under GNU Genral Public License version 2
+	#	http://www.gnu.org/licenses/gpl-2.0.html
+	#
+	###################
+
+	function rah_runtime($atts) {
+		global $rah_runtime;
+
+		extract(lAtts(array(
+			'format' => 1,
+			'index' => 0
+		),$atts));
+
+		if(!isset($rah_runtime[$index]) or empty($rah_runtime[$index])) {
+			$rah_runtime[$index] = microtime();
+			return;
+		}
+
+		if($format == 1)
+			$time = rtrim(number_format(microtime() - $rah_runtime[$index],15,'.',''),0);
+		else
+			$time = microtime() - $rah_runtime[$index];
+
+		$rah_runtime[$index] = '';
+		unset($GLOBALS['rah_runtime'][$index]);
+
+		return $time;
+	}
+?>
